44c56e8169a27ae5e962431a231c335a3fedd4ac
SLASH-7 <basnetujjwal98@gmail.com>
2023-07-22 23:16:14 +0545

    half

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <header>
-      <nav className="flex flex-wrap bg-nav justify-around items-center fixed h-20 w-full text-white ">
+      <nav className="flex flex-wrap bg-nav justify-around items-center fixed h-24 w-full text-white z-50">
         <div>
           <img src="./images/logo.png" alt="logo" />
         </div>
-        <div>
-          <ul className="md:flex hidden  gap-x-5 ">
+        <div className="text-lg">
+          <ul className="md:flex hidden  gap-x-10 ">
             <li>
               <a href="/">HOME</a>
             </li>
@@ -25,12 +25,13 @@ const Navbar = () => {
           </ul>
         </div>
         <ul
-          className={`md:hidden absolute bg-transparent rounded-bl-3xl w-2/4 h-[30rem]  top-16  shadow-2xl  text-center pt-10 transition-all ease-in duration-500
+          className={`md:hidden absolute bg-black bg-opacity-20
+           rounded-bl-full  w-2/4 h-screen   top-0  shadow-2xl  text-center pt-10 transition-all ease-in duration-500
         ${open ? "right-0" : "right-[-28rem]"}
         
         `}
         >
-          <li className="py-5 md:py-0 ">
+          <li className="py-5 md:py-0">
             <a href="/">HOME</a>
           </li>
           <li className="py-5 md:py-0">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,159 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <>
-      <div className="content md:h-screen h-[35rem]    bg-cover text-white w-full bg-[url('./image/background.png')]">
-        <div className="flex flex-col gap-y-10  items-center pt-56">
+      <div className="content md:h-screen h-[42rem] bg-cover text-white w-full bg-[url('./image/background.png')]">
+        <div className="flex flex-col gap-y-24  items-center pt-72">
           <p className=" md:text-6xl sm:text-5xl text-3xl w-2/3 text-center ">
             Have an amazing business meeting
             <span className="block md:text-2xl sm:text-xl text-lg pt-5 text-center">
               Small business with big dreams
             </span>
           </p>
-          <button className="border-2 border-white rounded-full px-10 text-xl py-2 ">
+          <button className="main border-2 border-white rounded-full px-10 text-xl py-2 ">
             Learn More
           </button>
         </div>
       </div>
-      <div className="bg-gray-500 h-96">
-        <h1>hi</h1>
+
+      <div className="flex flex-wrap  md:pt-40 sm:pt-20 pt-10  sm:pr-0 pr-5">
+        <div className="md:pl-56 md:pt-20 sm:pl-20 pl-10">
+          <img src="./images/text.png" alt="text" />
+        </div>
+        <div
+          className="md:pt-20 md:pl-36 sm:pl-20 sm:pt-20
+        pl-10 pt-10
+        "
+        >
+          <div className="flex mb-10">
+            <div>
+              <img src="./images/download (2).png" alt="" className="pt-3" />
+            </div>
+            <div className="max-w-[40rem] text-lg ml-5">
+              <p>Web Design</p>
+              <span>
+                Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+                enim ad minum veniaml.
+              </span>
+            </div>
+          </div>
+          <div className="flex mb-10">
+            <div>
+              <img src="./images/download (4).png" alt="" className="pt-3" />
+            </div>
+            <div className="max-w-[40rem] text-lg ml-5">
+              <p>Web Design</p>
+              <span>
+                Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+                enim ad minum veniaml.
+              </span>
+            </div>
+          </div>
+          <div className="flex mb-10">
+            <div>
+              <img src="./images/download (5).png" alt="" className="pt-3" />
+            </div>
+            <div className="max-w-[40rem] text-lg ml-5">
+              <p>Web Design</p>
+              <span>
+                Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+                enim ad minum veniaml.
+              </span>
+            </div>
+          </div>
+          <div className="flex mb-10">
+            <div>
+              <img src="./images/download (6).png" alt="" className="pt-3" />
+            </div>
+            <div className="max-w-[40rem] text-lg ml-5">
+              <p>Web Design</p>
+              <span>
+                Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+                enim ad minum veniaml.
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div className="bg-[url('./image/bg.png')] bg-cover h-[50rem]  flex flex-col flex-wrap justify-center pl-32 w-full bg-gray-200">
+        <div>
+          <p className="text-theme mb-10 text-4xl font-light">
+            Exceptional Customer Service
+          </p>
+        </div>
+        <div>
+          <p className="w-1/2 text-lg">
+            Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minum
+            veniaml.
+          </p>
+        </div>
+        <div>
+          <button className="main bg-theme py-2 px-4 text-white mt-10 text-xl">
+            Need to help? Lets Chat
+          </button>
+        </div>
+      </div>
+      <div className="h-2/3">
+        <div className="flex justify-center ">
+          <section>
+            <p className=" text-5xl pt-10 text-center">Our Portfolio</p>
+            <p className="pt-6">
+              Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed eiusmod
+            </p>
+          </section>
+        </div>
+        <div className="flex gap-x-5 justify-center mt-16 font-light">
+          <button className="bg-theme border-2 border-black px-2 text-2xl text-white">
+            ALL
+          </button>
+          <button className="hover:bg-theme border-2 border-black hover:text-white px-2 text-2xl">
+            WEB
+          </button>
+          <button className="hover:bg-theme border-2 border-black hover:text-white px-2 text-2xl">
+            UI/UX
+          </button>
+          <button className="hover:bg-theme border-2 border-black px-2 hover:text-white text-2xl">
+            PHOTOGRAPHY
+          </button>
+          <button className="hover:bg-theme border-2 border-black px-2 hover:text-white text-2xl">
+            BRANDING
+          </button>
+        </div>
+        <div className=" flex  justify-center mt-10 ">
+          <div className="scale w-80 h-80 overflow-hidden">
+            <img src="./images/gal1.png" alt="" className="object-cover" />
+          </div>
+          <div className="scale w-80 h-80 overflow-hidden">
+            <img src="./images/gal2.png" alt="" className="object-cover" />
+          </div>
+          <div className="scale w-80 h-80 overflow-hidden">
+            <img src="./images/gal3.png" alt="" className="object-cover" />
+          </div>
+          <div className="scale w-80 h-80 overflow-hidden">
+            <img src="./images/gal4.png" alt="" className="object-cover" />
+          </div>
+        </div>
+        <div className="flex justify-center mt-10 pb-20">
+          <div className="scale w-80 h-80 overflow-hidden">
+            <img src="./images/gal5.png" alt="" className="object-cover" />
+          </div>
+          <div className="scale  w-80 h-80 overflow-hidden ">
+            <img src="./images/gal6.png" alt="" className="object-cover" />
+          </div>
+          <div className="scale w-80 h-80 overflow-hidden">
+            <img src="./images/gal7.png" alt="" className="object-cover" />
+          </div>
+          <div className="scale w-80 h-80 overflow-hidden">
+            <img src="./images/gal8.png" alt="" className="object-cover" />
+          </div>
+        </div>
+      </div>
+      {/* <div className="bg-[url('./image/back1.png')] bg-cover w-full h-96"></div> */}
     </>
   );
 };
